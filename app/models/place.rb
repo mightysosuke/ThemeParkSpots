0ebefe01848ce589aa33f5257ac2d3fb5f0e5a47
sosuke <sosuke.sin@gmail.com>
2019-12-03 15:29:15 +0900

    座標から地点登録機能の実装

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ApplicationRecord
-  validates :address, presence: true
-  geocoded_by :address
-  after_validation :geocode
+  # validates :address, presence: true
+  # geocoded_by :address
+  # after_validation :geocode
+  reverse_geocoded_by :latitude, :longitude
+  after_validation :reverse_geocode
 end
