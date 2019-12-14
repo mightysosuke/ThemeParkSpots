f2bd2c969aa5a93dbc1abe9741039de907640850
sosuke <sosuke.sin@gmail.com>
2019-12-14 17:26:32 +0900

    新規投稿画面で、スポットの写真を投稿する機能を実装する#30

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -73,6 +73,6 @@ class PlacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
-      params.require(:place).permit(:name, :description, :address, :latitude, :longitude)
+      params.require(:place).permit(:name, :description, :address, :latitude, :longitude, images:[])
     end
 end
