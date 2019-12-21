1656a5ac0ded0047292a4cd97e0ffeedd7968fa2
sosuke <sosuke.sin@gmail.com>
2019-12-21 18:55:18 +0900

    自分の投稿を見れるようにする

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :comments
   get 'choices/new'
-  resources :places
   get 'spots/new'
   devise_for :users
   get 'pages/index'
+  get 'places/myplace'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
+
+
+  resources :comments
+  resources :places
 
 end
