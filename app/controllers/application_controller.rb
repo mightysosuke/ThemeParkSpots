daddcf6873f0d8eeba71a0efca6dbe7d03587d6d
sosuke <sosuke.sin@gmail.com>
2019-12-26 16:37:21 +0900

    【CSS】ユーザー情報変更画面の整備#83

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
       # devise_parameter_sanitizer.permit(:sign_up, keys: [:column])
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :account_id, :email])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:account_id])
-
+      devise_parameter_sanitizer.permit(:account_update, keys: [:email])
     end
 
 end
