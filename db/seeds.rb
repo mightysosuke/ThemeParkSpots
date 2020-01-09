17917d60acc675821e323477ab75f1c15dc17528
sosuke <sosuke.sin@gmail.com>
2020-01-09 10:49:10 +0900

    絵文字がDBに登録できない #128

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,71 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Area.create!(
+  [
+    {
+      park_id: 1,
+      name: 'メディテレーニアンハーバー'
+    },
+    {
+      park_id: 1,
+      name: 'アメリカンウォーターフロント'
+    },
+    {
+      park_id: 1,
+      name: 'ポートディスカバリー'
+    },
+    {
+      park_id: 1,
+      name: 'ロストリバーデルタ'
+    },
+    {
+      park_id: 1,
+      name: 'アラビアンコースト'
+    },
+    {
+      park_id: 1,
+      name: 'マーメイドラグーン'
+    },
+    {
+      park_id: 1,
+      name: 'ミステリアスアイランド'
+    },
+    {
+      park_id: 1,
+      name: 'プラザ'
+    },
+    {
+      park_id: 2,
+      name: 'ワールドバザール'
+    },
+    {
+      park_id: 2,
+      name: 'アドベンチャーランド'
+    },
+    {
+      park_id: 2,
+      name: 'ウエスタンランド'
+    },
+    {
+      park_id: 2,
+      name: 'クリッターカントリー'
+    },
+    {
+      park_id: 2,
+      name: 'ファンタジーランド'
+    },
+    {
+      park_id: 2,
+      name: 'トゥーンタウン'
+    },
+    {
+      park_id: 2,
+      name: 'トゥモローランド'
+    },
+    {
+      park_id: 2,
+      name: 'プラザ'
+    }
+  ]
+)
