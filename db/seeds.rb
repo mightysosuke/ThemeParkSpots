514fc9e7ba1138b7978698cf87ce0ab3dfce5551
sosuke <sosuke.sin@gmail.com>
2020-01-10 17:06:49 +0900

    マスタデータの投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Park.create!(
+  [
+    {
+      name: '東京ディズニーシー'
+    },
+    {
+      name: '東京ディズニーランド'
+    }
+  ]
+)
 Area.create!(
   [
     {
