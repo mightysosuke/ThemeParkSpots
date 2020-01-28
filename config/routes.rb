9cf9a559f1a9543280e58d7987faf60411a8c72c
sosuke <sosuke.sin@gmail.com>
2020-01-28 13:34:05 +0900

    aboutページを作成 #151
    作成とルーティングが完了

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   get 'pages/index'
+  get 'pages/about'
   get 'places/myplace'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
